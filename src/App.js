89702600f66736b5ee71713e96cc24d12e3a1457
pmita <panos.mita2@outlook.com>
2021-11-28 21:15:54 +0000

    Feature/Pages routed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+// ROUTING
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// PAGES
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from App.js component</h1>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+    return(
+        <div>
+            Hello from Login page component
+        </div>
+    );
+}
+
+export default Login;
