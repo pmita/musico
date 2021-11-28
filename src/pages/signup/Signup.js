ff47782c5fe660079227d90d61493ec26836b886
pmita <panos.mita2@outlook.com>
2021-11-28 22:07:20 +0000

    Feature/Signup form added

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
 
 const Signup = () => {
+    //STATE
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDisplayName] = useState('')
+
+    //EVENTS
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email, password, displayName)
+    }
+
     return(
-        <div>
-            Hello from Signup page component
+        <div className='signup-page'>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Email: </span>
+                    <input
+                        required
+                        type='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </label>
+                <label>
+                    <span>Password: </span>
+                    <input
+                        required
+                        type='password'
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </label>
+                <label>
+                    <span>Username: </span>
+                    <input
+                        required
+                        type='text'
+                        onChange={(e) => setDisplayName(e.target.value)}
+                        value={displayName}
+                    />
+                </label>
+                <button className='btn'>Sign Up</button>
+            </form>
         </div>
     );
 }
