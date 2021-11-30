304dc1964b49fbe78030a1c763d6d0710339b22d
pmita <panos.mita2@outlook.com>
2021-11-30 17:45:48 +0000

    Feature/Create page added, no firebase

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Create.css'
 
 const Create = () => {
+    // STATE
+    const [title, setTitle] = useState('')
+    const [artist, setArtist] = useState('')
+    const [cover, setCover] = useState('')
+    const [song_path, setSong_path] = useState('')
+
+    // EVENTS
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title, artist, cover, song_path)
+    }
+
     return(
-        <div>
-            welcome to Create page component
+        <div className='create-page'>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Song Title: </span>
+                    <input
+                        required
+                        type='text'
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <label>
+                    <span>Artist : </span>
+                    <input
+                        required
+                        type='text'
+                        onChange={(e) => setArtist(e.target.value)}
+                        value={artist}
+                    />
+                </label>
+                <label>
+                    <span>Album Cover: </span>
+                    <input
+                        required
+                        type='text'
+                        onChange={(e) => setCover(e.target.value)}
+                        value={cover}
+                    />
+                </label>
+                <label>
+                    <span>Song URL: </span>
+                    <input
+                        required
+                        type='text'
+                        onChange={(e) => setSong_path(e.target.value)}
+                        value={song_path}
+                    />
+                </label>
+                <button className='btn'>Add Song</button>
+            </form>
         </div>
     );
 }
